8c1fae6f63cddd82886eff76fdf0b08997b90841
Bry <112338618+Bry1k@users.noreply.github.com>
2024-12-22 22:44:49 -0800

    Update App Installer.bat
    - Added a new section (Development)
    - Added a internet connectivity test command

--- App Installer.bat
+++ App Installer.bat
@@ -29,7 +29,27 @@ if '%errorlevel%' NEQ '0' (
 
 
 cls
+
+:: Testing for connection in order to install Chocolately if needed.
+ping -n 2 www.google.com
+if errorlevel 1 goto fail
+if not errorlevel 1 goto success
+
+:fail
+cls
+echo No Internet Connection. Please make sure you are connected to the internet.
+timeout /t 3 >nul
+exit
+
+:success
+cls
+echo Internet connected
+timeout /t 3 >nul
+goto :choco
+
 :: Check to see if Chocolately path exists
+:choco
+cls
 set chocoPath=%ProgramData%\chocolatey\choco.exe
 
 if exist "%chocoPath%" (
@@ -156,7 +176,9 @@ echo                                                         3. HWINFO
 echo.
 echo                                     4. OBS Studio                              5. NanaZip
 echo.
-echo                                                                              Press N to go to the next page
+echo.                                    
+echo                                                      6. Notepad++
+echo                                                                               Press N to go to the next page                                   8. 
 set choice=
 set /p choice=Option: 
 if "%choice%"=="1" goto 7z
@@ -164,6 +186,7 @@ if "%choice%"=="2" goto gpuz
 if "%choice%"=="3" goto hwinfo
 if "%choice%"=="4" goto obs
 if "%choice%"=="5" goto nz
+if "%choice%"=="6" goto n++
 if "%choice%"=="N" (
     goto game
 ) else if "%choice%"=="n" (
@@ -214,17 +237,17 @@ echo.
 echo.
 echo                                 3. Ubisoft Connect                            4. GOG Galaxy                                 
 echo.
-echo                                                                               Press E to exit 
+echo                                                                               Press N to go to next page 
 set choice=
 set /p choice=Option: 
 if "%choice%"=="1" goto eg
 if "%choice%"=="2" goto steam 
 if "%choice%"=="3" goto uc 
 if "%choice%"=="4" goto gog 
-if "%choice%"=="E" (
-    goto exit
-) else if "%choice%"=="e" (
-    goto exit
+if "%choice%"=="N" (
+    goto dev
+) else if "%choice%"=="n"(
+    goto dev
 )
 
 echo Invalid Option. Please select a valid option.
@@ -245,6 +268,65 @@ goto game
 :gog
 choco install goggalaxy
 goto game
+
+
+:dev
+cls
+echo.
+echo.
+echo.
+echo.
+echo                                                       Development
+echo.
+echo.
+echo                                 1. Python                                 2. Git
+echo.
+echo                                 3. Rust                                   4. Golang
+echo.
+echo                                 5. Visual Studio Code                     6. VSCodium
+echo.
+echo                                                                         Press E to exit
+set choice=
+set /p choice=Option: 
+if "%choice%"=="1" goto py
+if "%choice%"=="2" goto git
+if "%choice%"=="3" goto rs
+if "%choice%"=="4" goto gl
+if "%choice%"=="5" goto vsc
+if "%choice%"=="6" goto vscd 
+if "%choice%"=="E" goto (
+    goto exit
+) else if "%choice%"=="e" (
+    goto exit
+)
+echo Invalid Option. Please select a valid option.
+goto dev
+
+:py
+choco install python311
+goto dev
+
+:git
+choco install git.install
+goto dev
+
+:rs
+choco install rust
+goto dev
+
+:gl 
+choco install golang
+goto dev
+
+:vsc
+choco install vscode
+goto dev
+
+:vscd
+choco install vscodium
+goto dev
+
+
 
 :exit
 :: Pop up message once user finishes
